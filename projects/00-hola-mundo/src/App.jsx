319f48230cca9986eb6b86f9492adf92d5511866
Joshua <hola@joshua.cl>
2024-01-10 19:40:57 -0300

    Creacion base proyecto Twitter Card

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,20 @@
+export const App = () => {
+  return (
+    <article>
+
+      <header>
+        <img src="https://unavatar.io/midudev" alt="El avatar del usuario" />
+        <div>
+          <strong>Miguel Ángel Durán</strong>
+          <span>@midudev</span>
+        </div>
+      </header>
+
+      <aside>
+        <button>
+          Seguir
+        </button>
+      </aside>
+    </article>
+  )
+}
