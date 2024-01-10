d8a6a792ab395a9fb94ddf1980bb6d3afdbabd9b
Joshua <hola@joshua.cl>
2024-01-10 19:56:33 -0300

    Agregando estilos

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,20 +1,8 @@
+import './App.css';
+import { TwitterFollowCard } from "./TwitterFollowCard"
+
 export const App = () => {
   return (
-    <article>
-
-      <header>
-        <img src="https://unavatar.io/midudev" alt="El avatar del usuario" />
-        <div>
-          <strong>Miguel Ángel Durán</strong>
-          <span>@midudev</span>
-        </div>
-      </header>
-
-      <aside>
-        <button>
-          Seguir
-        </button>
-      </aside>
-    </article>
+    <TwitterFollowCard />
   )
 }
