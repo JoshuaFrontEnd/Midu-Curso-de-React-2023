5174f9c6419e5b9d46c71492a58dfc8a50f40c11
Joshua <hola@joshua.cl>
2024-01-11 16:44:33 -0300

    Explicacion de las 'keys' en React

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -7,32 +7,94 @@ export const App = () => {
   const format = ( userName ) => `@${ userName }`;
 
   // Otra forma de pasar las props, como un objeto y luego declararla en el componente con el operador "spread", esta forma no se recomienda, ya que la potencia de React esta en ser lo mas declarativo posible
-  const lastCompProps = { formatUserName: format, userName: "github", name: "GitHub" };
+  // const lastCompProps = { formatUserName: format, userName: "github", name: "GitHub" };
+
+  // return (
+  //   <section className='App'>
+
+  //     <TwitterFollowCard
+  //       formatUserName={ format }
+  //       initialIsFollowing={ true }
+  //       userName="midudev"
+  //       name="Miguel Ángel Durán"
+  //     />
+
+  //     <TwitterFollowCard
+  //       formatUserName={ format }
+  //       userName="YouTube"
+  //       name="YouTube"
+  //     />
+
+  //     <TwitterFollowCard
+  //       formatUserName={ format }
+  //       userName="reactjs"
+  //       name="React"
+  //     />
+
+  //     <TwitterFollowCard { ...lastCompProps } />
+
+  //   </section>
+  // )
+
+  // Usando un JSON para renderizar la informacion en un componente y explicar el tema de las "keys" en React
+  const users = [
+    {
+      userName: 'midudev',
+      name: 'Miguel Ángel Durán',
+      isFollowing: true
+    },
+    {
+      userName: 'YouTube',
+      name: 'YouTube',
+      isFollowing: false
+    },
+    {
+      userName: 'reactjs',
+      name: 'React',
+      isFollowing: true
+    },
+    {
+      userName: 'github',
+      name: 'GitHub',
+      isFollowing: false
+    }
+  ]
 
   return (
     <section className='App'>
+      {
+        // users.map( user => {
 
-      <TwitterFollowCard
-        formatUserName={ format }
-        initialIsFollowing={ true }
-        userName="midudev"
-        name="Miguel Ángel Durán"
-      />
+        //   const { userName, name, isFollowing } = user;
 
-      <TwitterFollowCard
-        formatUserName={ format }
-        userName="YouTube"
-        name="YouTube"
-      />
+        //   return (
 
-      <TwitterFollowCard
-        formatUserName={ format }
-        userName="reactjs"
-        name="React"
-      />
+        //     <TwitterFollowCard
+        //       key={ userName }
+        //       formatUserName={ format }
+        //       userName={ userName }
+        //       name={ name }
+        //       initialIsFollowing={ isFollowing }
+        //     />
 
-      <TwitterFollowCard { ...lastCompProps } />
+        //   )
 
+        // })
+
+        /* Refactorizacion ( optimización ) del codigo anterior */
+        users.map( ({ userName, name, isFollowing }) => (
+
+          <TwitterFollowCard
+            key={ userName }
+            formatUserName={ format }
+            userName={ userName }
+            name={ name }
+            initialIsFollowing={ isFollowing }
+          />
+
+        ))
+
+      }
     </section>
   )
 }
@@ -47,4 +109,6 @@ export const App = () => {
   * En React existen los Componentes y los Elementos, basicamente los componentes son los que fabrican los elementos, y los elementos son los que se renderizan
 
   * Las propiedades en React deben ser inmutables, es decir, no deben modificarse, porque eso seria modificar el estado real de las cosas, lo que si se puede hacer es crear una copia de la propiedad y esa copia modificarla
+
+  * La propiedad "key" en React sirve para identificar los elementos de una iteracion, el valor de esta propiedad debe ser unica para que React pueda realizar un seguimiento de que elementos han sido modificados de manera mas optima, si no se declara o no es unica, react podria renderizar el componente de iteracion con problemas. Algunas recomendaciones es no usar datos aleatorios como valor de "key", por ejemplo usando la funcion Math.random() o Date.now(), o usar el index, lo ideal seria usar un id de la informacion que venga ya seteada desde una base de datos, o identificar valores no repetibles unicos como el nombre de usuario, etc.
 ---------------------------------------------------------------- */
