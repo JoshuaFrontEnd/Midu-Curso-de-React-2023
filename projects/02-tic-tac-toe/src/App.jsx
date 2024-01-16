c8f8f182c5b4191ad006713a8997e3a22f9edd61
Joshua <hola@joshua.cl>
2024-01-16 20:21:57 -0300

    Dibujando el tablero

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,6 +1,49 @@
+// Creando los turnos
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+// Creando el tablero
+// (Creo un array con 9 posiciones y relleno las 9 posiciones con valores null )
+const board = Array( 9 ).fill( null );
+
+const Square = ({ children, updateBoard, index }) => {
+
+  return (
+    <div className="square">
+      { children }
+    </div>
+  )
+
+}
+
 function App() {
 
-  return <h1>Tic tac toe</h1>
+  return (
+    <main className="board">
+      <h1>Tic tac toe</h1>
+
+      {/* Renderizo el array */}
+      <section className="game">
+        {
+          board.map( ( _, index ) => {
+
+            return (
+              <Square
+                key={ index }
+                index={ index }
+              >
+                { index }
+              </Square>
+            )
+
+          })
+        }
+      </section>
+    </main>
+  )
+
 }
 
 export default App
