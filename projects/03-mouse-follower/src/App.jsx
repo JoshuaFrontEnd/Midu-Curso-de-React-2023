a26e415422163c28c92a3fc55db16ec2d41cbc0f
Joshua <hola@joshua.cl>
2024-01-27 20:51:23 -0300

    Creacion boton que activa/desactiva el seguir el cursor

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,7 +1,19 @@
-function App() {
+import { useEffect, useState } from 'react'
+
+function App () {
+  const [enabled, setEnabled] = useState(false)
+
+  useEffect(() => {
+    console.log('effect: ', { enabled })
+  }, [enabled])
 
   return (
-    <h3>Proyecto 3</h3>
+    <main>
+
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </main>
   )
 }
 
