69d3635a8bc0080f994ea4ad254fbe0246a0373a
Joshua <hola@joshua.cl>
2024-01-27 21:01:14 -0300

    Explicacion Hook useEffect

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -3,8 +3,12 @@ import { useEffect, useState } from 'react'
 function App () {
   const [enabled, setEnabled] = useState(false)
 
+  // useEffect es un Hook que nos permite ejecutar codigo arbitrario cuando un componente se "monta" en el DOM y cada vez que cambien las dependencias que nosotros configuremos
   useEffect(() => {
+    // Acá se va a ejecutar el log, cuando el componente se monte en el DOM
     console.log('effect: ', { enabled })
+
+    // Y se volvera a ejecutar cada vez que el valor de la dependencia "enabled" cambie, si queremos que el log se ejecute solo cuando se monte el componente, hay que declarar la dependencia de cambio con un arreglo vacio []
   }, [enabled])
 
   return (
