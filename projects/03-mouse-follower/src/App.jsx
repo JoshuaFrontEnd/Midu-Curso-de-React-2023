fa1da61a8a2d2236df9a53680c5a9132caa0c7b5
Joshua <hola@joshua.cl>
2024-01-27 21:15:40 -0300

    Detectando la posicion y activando el seguimiento del cursor

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -3,10 +3,23 @@ import { useEffect, useState } from 'react'
 function App () {
   const [enabled, setEnabled] = useState(false)
 
+  // Seteando el estado la posicion inicial del cursor en 0
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
   // useEffect es un Hook que nos permite ejecutar codigo arbitrario cuando un componente se "monta" en el DOM y cada vez que cambien las dependencias que nosotros configuremos
   useEffect(() => {
     // Acá se va a ejecutar el log, cuando el componente se monte en el DOM
     console.log('effect: ', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove: ', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
 
     // Y se volvera a ejecutar cada vez que el valor de la dependencia "enabled" cambie, si queremos que el log se ejecute solo cuando se monte el componente, hay que declarar la dependencia de cambio con un arreglo vacio []
   }, [enabled])
@@ -23,7 +36,7 @@ function App () {
         top: -20,
         width: 40,
         height: 40,
-        transform: 'translate( 0px, 0px )'
+        transform: `translate( ${position.x}px, ${position.y}px )`
       }}
       />
       <button onClick={() => setEnabled(!enabled)}>
