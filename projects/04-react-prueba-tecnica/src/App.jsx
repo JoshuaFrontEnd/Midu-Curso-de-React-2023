139c09dd7b47919b14bdbdd8d97f528682cb2c92
Joshua <hola@joshua.cl>
2024-01-28 22:22:11 -0300

    Recuperando un hecho aleatorio de gatos de la primera API

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
 
 export const App = () => {
-  const [fact, setFact] = useState('lorem ipsum cat fact whatever')
+  const [fact, setFact] = useState()
+
+  // No puedes usar React Quer, SWR, axios, apollo, etc
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => setFact(data.fact))
+  }, [])
 
   return (
     <main>
       <h1>App de gatitos</h1>
-      <p>{fact}</p>
+      {fact && <p>{fact}</p>}
     </main>
   )
 }
