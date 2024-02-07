52620940e9823e5a02bd53401e414fd05a1be597
Joshua <hola@joshua.cl>
2024-02-07 20:02:49 -0300

    Listando las peliculas, mostrando titulo, año y poster

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,6 +1,14 @@
 import './App.css'
+import responseMovies from './mocks/with-results.json'
+import withoutResults from './mocks/no-results.json'
 
 function App() {
+  // Cuanto tenemos peliculas, cuando tenemos un Search y es un array
+  // Por lo tanto las peliculas se encuentran en 'responseMovies.Search
+  const movies = responseMovies.Search
+
+  // Cuando sabemos que hay peliculas, cuando movie tiene el "length" mayor a cero
+  const hasMovies = movies.length > 0
 
   return (
     <div className='page'>
@@ -14,7 +22,29 @@ function App() {
       </header>
 
       <main>
-        Aquí irán los resultados
+        {
+          /* Renderizado condicional */
+          /* Si tiene peliculas, Lista las películas y muestra el título, año y poster */
+          hasMovies
+          ? (
+              <ul>
+                {
+                  movies.map((movie) => (
+                    <li key={ movie.imdbID }>
+                      <h3>{movie.Title}</h3>
+                      <p>{movie.Year}</p>
+                      <img src={movie.Poster} alt={movie.Title} />
+                    </li>
+                  ))
+                }
+              </ul>
+            )
+          /* Si no tiene peliculas, mostrar un mensaje */
+          : (
+              <p>No se encontraron resultados para esa búsqueda</p>
+            )
+        }
+
       </main>
 
     </div>
