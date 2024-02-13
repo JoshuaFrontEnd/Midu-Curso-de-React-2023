f748d92574e7906fd838e9f329cbf9a77f74d5d1
Joshua <hola@joshua.cl>
2024-02-12 21:27:04 -0300

    Manejando el formulario con useRef

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,18 +1,28 @@
 import './App.css'
 import { useMovies } from './hooks/useMovies'
 import { Movies } from './components/Movies'
+import { useRef } from 'react'
 
 function App() {
 
   const { movies } = useMovies()
+  const inputRef = useRef()
+
+  const handleSubmit = ( event ) => {
+    event.preventDefault()
+
+    const inputEl = inputRef.current
+    const value = inputEl.value
+    console.log(value);
+  }
 
   return (
     <div className='page'>
 
       <header>
         <h1>Buscador de películas</h1>
-        <form className='form'>
-          <input placeholder='Avengers, Star Wars, The Matrix...' />
+        <form className='form' onSubmit={ handleSubmit }>
+          <input ref={ inputRef } placeholder='Avengers, Star Wars, The Matrix...' />
           <button type='submit'>Buscar</button>
         </form>
       </header>
@@ -26,3 +36,10 @@ function App() {
 }
 
 export default App
+
+
+/* --------------------------------------------------------------
+  - Hook useRef:
+
+  Te permite crear una referencia mutable que persiste durante todo el ciclo de vida del componente, y lo que es mejor que es muy util para guardar cualquier valor que puedas mutar, como un identificador, un elemento del DOM, un contador, etc, y que cada vez que cambia no vuelve a renderizar el componente
+-------------------------------------------------------------- */
